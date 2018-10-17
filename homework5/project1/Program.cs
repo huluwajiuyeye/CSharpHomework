a0a2f12fe20409566706219ac6daf5c43681c5f8
Zhou Xingqi <401360663@qq.com>
2018-10-17 12:29:53 +0800

    homework5

---
+++ homework5/project1/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    public class Order
+    {
+        public void Start(OrderService a)
+        {
+            bool c = true;
+            while (c)
+            {
+                Console.WriteLine("请输入 1：添加订单");
+                Console.WriteLine("       2：删除订单");
+                Console.WriteLine("       3：更改订单");
+                Console.WriteLine("       4：查找订单");
+                Console.WriteLine("       5：浏览订单");
+                Console.WriteLine("       6：结束操作");
+                string b = Console.ReadLine();
+                switch (b)
+                {
+                    case "1": a.Addorder(); break;
+                    case "2": a.DeletList(); break;
+                    case "3": a.ChangeList(); break;
+                    case "4": a.FindList(); break;
+                    case "5": a.ShowList(); break;
+                    case "6": c = false; break;
+                    default:
+                        Console.WriteLine("大哥守点规矩好吧");
+                        break;
+
+                }
+            }
+        }
+        public class OrderDetails : Object
+        {
+            public String id;
+            public String name;
+            public String customer;
+            public int money;
+            public override string ToString()
+            {
+                return "商品编号为:" + id + "\t商品名称：" + name + "\t顾客姓名：" + name+"\t商品金额："+money;
+            }
+
+        }
+        public class OrderService
+        {
+            public List<OrderDetails> testList = new List<OrderDetails>();
+            public void Addorder()
+            {
+                OrderDetails a = new OrderDetails();
+                Console.WriteLine("请依次输入商品编号、商品名字、客户姓名、商品金额");
+                a.id = Console.ReadLine();       
+                a.name = Console.ReadLine();
+                a.customer = Console.ReadLine();
+                a.money = Convert.ToInt32(Console.ReadLine());
+                testList.Add(a);
+            }
+            public void ShowList()
+            {
+                foreach (OrderDetails s in testList)
+                {
+                    Console.WriteLine(s.ToString());
+                }
+            }
+            public void DeletList()
+            {
+                try
+                {
+                    Console.WriteLine(" 请输入删除第几个：");
+                    string a = Console.ReadLine();
+                    int b = Convert.ToInt32(a);
+                    testList.RemoveAt(b - 1);
+                }
+                catch { Console.WriteLine("请输入阿拉伯数字，并删除存在的数据"); }
+            }
+            public void ChangeList()
+            {
+                OrderDetails a = new OrderDetails();
+                Console.WriteLine("输入需要更改的商品编号：");
+                a.id =Console.ReadLine();
+                foreach (OrderDetails s in testList)
+                {
+                    if (a.id == s.id)
+                    {
+                        Console.WriteLine("输入新的商品名字、客户名字、商品金额");
+                        s.name = Console.ReadLine();
+                        s.customer = Console.ReadLine();
+                        s.money = Convert.ToInt32(Console.ReadLine());
+                    }
+                };
+            }
+            public void FindList()
+            {
+                OrderDetails a = new OrderDetails();
+                Console.WriteLine("请输入 1：按单号查找\t2：按商品名字\t3：按客户查找\t4：查找金额大于10000的订单");
+                string b = Console.ReadLine();
+                switch (b)
+                {
+                    case "1":
+                        Console.WriteLine("输入商品单号：");
+                        a.id = Console.ReadLine();
+                        var i = from n in testList where n.id==a.id select n;
+                        foreach(OrderDetails od in i)
+                        {
+                            Console.WriteLine(od.ToString());
+                        }                                       
+                        break;
+                    case "2":
+                        Console.WriteLine("输入商品名字：");
+                        a.name = Console.ReadLine();
+                        var x = from n in testList where n.name == a.name select n;
+                        foreach (OrderDetails od in x)
+                        {
+                            Console.WriteLine(od.ToString());
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("输入客户名字：");
+                        a.customer = Console.ReadLine(); foreach (OrderDetails s in testList)
+                        {
+                            if (a.name == s.name) { Console.WriteLine("订单编号: " + s.id + " 商品名称: " + s.name + " 客户: " + s.customer); }
+                        };
+                        break;
+                    case "4":
+                        var m = from n in testList where n.money > 10000 select n;
+                        foreach (OrderDetails od in m)
+                        {
+                            Console.WriteLine(od.ToString());
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("按规则出牌");
+                        break;
+                }
+
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Order order = new Order();
+                OrderService a = new OrderService();
+                order.Start(a);
+            }
+
+        }
+    }
+}
---
+++ homework5/tree/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "30";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "20";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "左侧树杈角度：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "右侧树杈角度：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 112);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "0.6";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "左侧树杈长度：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "右侧树杈长度：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(96, 146);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "0.6";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 434);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
